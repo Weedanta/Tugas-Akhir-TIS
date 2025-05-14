0ff58704c8e6d4831abdd0cf538d117579ee60ef
olfat <code.olfatfaiz@gmail.com>
2025-05-14 09:45:47 +0700

    Rick Roll IFrame Modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    
     <div className="flex flex-col items-center justify-center h-full w-full bg-gray-100">
-      <iframe className="h-full w-full aspect-video" src="https://www.youtube.com/embed/xvFZjo5PgG0" title="Apapun" />
+      {/* add autoplay */}
+      <iframe className="h-full w-full aspect-video" src="https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1" title="Apapun" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen />
     </div>
   )}
